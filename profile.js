d687d2bf75703fb311fb68eafe212b2bc7f574ba
Calvin Froedge <calvinfroedge@gmail.com>
2013-10-23 13:44:35 -0500

    v0.0.0

---
+++ profile.js
@@ -0,0 +1,67 @@
+angular.module('10digit.profile', ['ui.mask', '10digit.utils', 'ui.validate', 'ui.bootstrap.tooltip', 'app.config'])
+
+.factory('ProfileConfig', function(){
+    var config = {
+        testMode: false,
+        initialValues: {
+            profile:
+        }
+    };
+    return config;
+})
+
+.factory('Profile', ['$ajax', function($ajax){
+    var profile = {}
+
+    return {
+        profile: profile,
+	    load: function(){
+			$ajax.run('members/me/profile', {
+				success: function(res, status){
+					profile.name = res.name;
+					profile.email = res.email;
+					profile.phone = res.phones[0];
+				}
+			})
+	    },
+	    update: function(opts){
+		    var put = angular.copy(profile);
+			$ajax.run('members/me/profile', {
+				method: 'PUT',
+				data: put,
+				success: function(res, status){
+					if(opts.success) opts.success(res);
+				}
+			});
+		}
+    }
+}])
+
+.controller('ProfileCtrl', ['$scope', '$attrs', 'Profile', 'ProfileConfig', function($scope, $attrs, Profile, Config){
+	if($scope.signup && Config.testMode){
+		$scope.profile = Config.initialValues.profile;
+	}
+
+	if(!$scope.signup){
+		Profile.load();
+		$scope.profile = Profile.profile;
+	}
+
+	$scope.update = function(){
+		Profile.update({
+			success: function(){
+				$scope.success = "Profile updated successfully.";
+			}
+		});
+	}
+}])
+
+.directive('profile', function () {
+  return {
+    restrict:'E',
+    controller:'ProfileCtrl',
+    transclude: true,
+    replace: false,
+    templateUrl: 'template/10digit/profile.html'
+  };
+});
